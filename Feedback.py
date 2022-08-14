22599a4db1dbb589aac4d970c377e1a6344bec50
sam joshua <samjoshuacs2002@gmail.com>
2022-08-14 13:24:34 +0530

    123

---
+++ Feedback.py
@@ -0,0 +1,11 @@
+ans=[]
+for i in range(int(input())):
+    a=input()
+    b="101"
+    c="010"
+    if b in a or c in a:
+        ans.append('Good')
+    else:
+        ans.append('Bad')
+for j in ans:
+    print(j)        
--- Kivy/1st program.py
+++ Kivy/1st program.py
@@ -1,26 +1,39 @@
+# code to use the .kv file as a string in the main file
+# code how to use .kv file in kivy
+ 
+# import kivy module
 import kivy
+ 
+# base Class of your App inherits from the App class.
+# app:always refers to the instance of your application
 from kivy.app import App
-from kivy.uix.label import Label
-  
-  
-# Replace this with your 
-# current version
-kivy.require('1.11.1')  
-  
-# Defining a class
-class MyFirstKivyApp(App):
-      
-    # Function that returns 
-    # the root widget
+ 
+# it is to import Builder
+from kivy.lang import Builder
+ 
+# this restrict the kivy version i.e
+# below this kivy version you cannot use the app or software
+# not compulsory to write it
+kivy.require('1.9.1')
+ 
+# building kv file as string
+kvfile = Builder.load_string("""
+Label:
+    text:
+        ('[b]Hello[/b] [color = ff0099]World[/color]\\n'
+        '[color = ff0099]Hello[/color] [b]World[/b]\\n'
+        '[b]Hello[/b] [color = ff0099]World:):)[/color]')
+    markup: True
+    font_size: '64pt'
+""")
+ 
+# define the App class
+# and just pass rest write on kvfile
+# not necessary to pass
+# can also define function in it
+class kvfileApp(App):
     def build(self):
-          
-        # Label with text Hello World is 
-        # returned as root widget
-        return Label(text ="Hello World !")          
-  
-  
-# Here our class is initialized
-# and its run() method is called. 
-# This initializes and starts 
-# our Kivy application.
-MyFirstKivyApp().run()
+        return kvfile
+ 
+kv = kvfileApp()
+kv.run()
