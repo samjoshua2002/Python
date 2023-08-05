3d879862b5989ef05efc9d2e4b574a186e273b8f
Sam Joshua P <samjoshuacs2002@gmail.com>
2023-08-05 07:49:49 +0530

    done

---
+++ binary.py
@@ -0,0 +1,12 @@
+n=int(input('please enter the no. in decimal format: '))
+x=n
+k=[]
+while (n>0):
+    a=int(float(n%2))
+    k.append(a)
+    n=(n-a)/2
+k.append(0)
+string=""
+for j in k[::-1]:
+    string=string+str(j)
+print('The binary no. for %d is %s'%(x, string))
